ba543ad354e28e221bbd170c51e174f2cebf7f69
Surya Kalyan <annakalyan286@gmail.com>
2024-06-12 19:58:07 +0530

    Problem 3 - Kadanes Algorithm (Max Sub Array)

---
+++ src/com/blind75/leetcode/qstns/arrays/KadanesAlgorithmMaxSubArray.java
@@ -0,0 +1,62 @@
+package com.blind75.leetcode.qstns.arrays;
+
+public class KadanesAlgorithmMaxSubArray {
+
+    public static void main(String[] args) {
+
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4, -2, 9};
+
+        // BruteForce Approach - TimeComplexity : 0[N^3]
+        System.out.println(getMaxSubArrayBruteForceApproach(arr));
+
+        // Better Appraoch - TimeComplexity : 0[N^2]
+        System.out.println(getMaxSubArrayBetterApproach(arr));
+
+        // Optimal/Best Appraoch - TimeComplexity : 0[N]
+        System.out.println(getMaxSubArrayUsingKadanesAlgoApproach(arr));
+    }
+
+    private static int getMaxSubArrayBruteForceApproach(int[] arr) {
+
+        int maxSumValue = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                int sum = 0;
+                for (int k = i; k <= j; k++) {
+                    sum += arr[k];
+                }
+
+                maxSumValue = Math.max(maxSumValue, sum);
+            }
+        }
+        return maxSumValue;
+    }
+
+    private static int getMaxSubArrayBetterApproach(int[] arr) {
+
+        int maxSumValue = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            for (int j = i; j < arr.length; j++) {
+                sum += arr[j];
+                maxSumValue = Math.max(maxSumValue, sum);
+            }
+        }
+        return maxSumValue;
+    }
+
+    private static int getMaxSubArrayUsingKadanesAlgoApproach(int[] arr) {
+
+        int sum = arr[0];
+        int maxSumValue = Integer.MIN_VALUE;
+        for (int i = 1; i < arr.length; i++) {
+            sum += arr[i];
+            if (sum < 0) {
+                sum = 0;
+            }
+            maxSumValue = Math.max(maxSumValue, sum);
+        }
+        return maxSumValue;
+    }
+}
