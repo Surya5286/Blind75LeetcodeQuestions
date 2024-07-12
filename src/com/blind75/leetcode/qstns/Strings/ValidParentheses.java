704198b7ac43b6dd21d66eaf8fac4f427d43500e
Surya Kalyan <annakalyan286@gmail.com>
2024-07-12 19:56:13 +0530

    formatted ValidParentheses.java class

--- src/com/blind75/leetcode/qstns/Strings/ValidParentheses.java
+++ src/com/blind75/leetcode/qstns/Strings/ValidParentheses.java
@@ -11,7 +11,7 @@ public class ValidParentheses {
         List<Boolean> booleanList = List.of(true, true, true, false, true, false, false);
         List<Boolean> validParentheses = List.of(isValidParentheses(str1), isValidParentheses(str2), isValidParentheses(str3), isValidParentheses(str4), isValidParentheses(str5), isValidParentheses(str6), isValidParentheses(str7));
 
-        System.out.println("Input String : " + str5 + "\tIs ValidParentheses : " +  isValidParentheses(str5));
+        System.out.println("Input String : " + str5 + "\tIs ValidParentheses : " + isValidParentheses(str5));
 
         System.out.println("*****************************************************");
 
@@ -19,38 +19,32 @@ public class ValidParentheses {
 
 
         if (booleanList.equals(validParentheses))
-            System.out.println("Successfully evaluated all the samples and the results are as expected : "
-            + booleanList);
+            System.out.println("Successfully evaluated all the samples and the results are as expected : " + booleanList);
         else
-            System.out.println("Successfully evaluated all the samples and the results are not as expected. " +
-                    "\nExpected List of results : "+ booleanList +
-                    "\nActual List of results : "+ validParentheses);
+            System.out.println("Successfully evaluated all the samples and the results are not as expected. " + "\nExpected List of results : " + booleanList + "\nActual List of results : " + validParentheses);
 
         System.out.println("*****************************************************");
 
     }
 
+    /**
+     * @param str - Incoming Patntheses String
+     * @return - Returns the Boolean Value [true, false]
+     */
     private static boolean isValidParentheses(String str) {
 
         Stack<Character> charStack = new Stack<>();
 
         for (char ch : str.toCharArray()) {
-
             if (ch == '(' || ch == '[' || ch == '{') {
-
                 charStack.push(ch);
-
             } else {
                 if (charStack.isEmpty()) return false;
                 char popChar = charStack.pop();
-
-                if (! ((ch == ')' && popChar == '(') || (ch == ']' && popChar == '[') || (ch == '}' && popChar == '{')))
-                   return false;
-
+                if (!((ch == ')' && popChar == '(') || (ch == ']' && popChar == '[') || (ch == '}' && popChar == '{')))
+                    return false;
             }
         }
-
-
         return charStack.isEmpty();
     }
 }
