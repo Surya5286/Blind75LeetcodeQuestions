7f888de2b0e7c21b3ceec31a22f61b4f5422b244
sanna <annakalyan286@gmail.com>
2026-01-25 15:31:34 +0530

    Heap updates & BucketSort Optimized approach added.

--- src/com/blind75/leetcode/qstns/heap/TopKFrequentElements.java
+++ src/com/blind75/leetcode/qstns/heap/TopKFrequentElements.java
@@ -2,26 +2,46 @@ package com.blind75.leetcode.qstns.heap;
 
 import java.util.*;
 
+/*
+    Heap is usually better when k is small;
+    bucket sort is theoretically linear but can cost more memory and object allocations.
+
+    Heap Approach is recommended.
+*/
+
 public class TopKFrequentElements {
 
     public static void main(String[] args) {
 
-        int[] nums = {1, 1, 1, 2, 2, 3};
+        int[] numArray = {1, 1, 1, 2, 2, 3};
         int k = 2;
-        int[] result = topKFrequentElementsUsingMinHeapBetterApproach(nums, k);
-        System.out.println("Top " + k + " frequent elements: " + Arrays.toString(result)); // Output: [1, 2]
+        int[] result = topKFrequentElementsUsingMinHeapBetterApproach(numArray, k);
+        System.out.println("Top " + k + " frequent elements (Min Heap): " + Arrays.toString(result)); // Output: [1, 2]
 
-        int[] nums2 = {4, 1, -1, 2, -1, 2, 3};
+        int[] numArray2 = {4, 1, -1, 2, -1, 2, 3};
         int k2 = 2;
-        int[] result2 = topKFrequentElementsUsingMinHeapBetterApproach(nums2, k2);
-        System.out.println("Top " + k2 + " frequent elements: " + Arrays.toString(result2)); // Output: [ -1, 2]
+        int[] result2 = topKFrequentElementsUsingMinHeapBetterApproach(numArray2, k2);
+        System.out.println("Top " + k2 + " frequent elements (Min Heap): " + Arrays.toString(result2)); // Output: [ -1, 2]
+
+        System.out.println("--------------------------------------------------");
+        System.out.println("Optimal Approach using Bucket Sort");
+        System.out.println("--------------------------------------------------");
+
+        // Another Approach
+        int[] result3 = topKFrequentElementsUsingBucketSortOptimalApproach(numArray, k);
+        System.out.println("Top " + k + " frequent elements (BucketSort): " + Arrays.toString(result3)); // Output: [1, 2]
+
+        int[] result4 = topKFrequentElementsUsingBucketSortOptimalApproach(numArray2, k2);
+        System.out.println("Top " + k2 + " frequent elements (BucketSort): " + Arrays.toString(result4)); // Output: [ -1, 2]
     }
 
     /*
         Complexity (what to say)
             Building frequency map: O(n)
             Heap operations: O(m log k) where m = #unique elements
-            Time Complexity :- O(n + m log k) or O(n log k) in worst case when all elements are unique
+            Time = O(n + m log k) where m <= n
+                    Worst case m = n → O(n log k)
+                    If k ≈ n → O(n log n)
             Space Complexity :- O(m + k) ~ O(m) for frequency map and O(k) for heap
     */
     private static int[] topKFrequentElementsUsingMinHeapBetterApproach(int[] numArray, int k) {
@@ -29,9 +49,6 @@ public class TopKFrequentElements {
         // Edge cases
         if (numArray == null || numArray.length == 0 || k <= 0)
             return new int[0];
-
-        if (k == numArray.length)
-            return numArray;
 
         // Frequency map to count occurrences of each element
         Map<Integer, Integer> frequencyMap = new HashMap<>((int) (numArray.length / 0.75f) + 1); // Initial capacity to avoid rehashing
@@ -60,6 +77,54 @@ public class TopKFrequentElements {
 
         for (int i = capacity-1 ; i >= 0 ; i--)
             result[i] = minHeap.poll().getKey();
+
+        return result;
+    }
+
+    /*
+       Complexity (what to say)
+            Building frequency map: O(n)
+            Bucket sort operations: O(n)
+            Time Complexity :- O(n)
+            Space Complexity :- O(n) for frequency map and buckets
+   */
+    @SuppressWarnings("unchecked")
+    private static int[] topKFrequentElementsUsingBucketSortOptimalApproach(int[] numArray, int k) {
+
+        // Edge cases
+        if (numArray == null || numArray.length == 0 || k <= 0)
+            return new int[0];
+
+        Map<Integer, Integer> frequencyMap = new HashMap<>((int) (numArray.length / 0.75f) + 1); // Initial capacity to avoid rehashing
+
+        for (int num : numArray)
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+
+        // Create buckets where index represents frequency
+        List<Integer>[] buckets = new List[numArray.length + 1];
+
+        // Fill the buckets
+        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
+            int frequency = entry.getValue();
+            if (buckets[frequency] == null)
+                buckets[frequency] = new ArrayList<>();
+            buckets[frequency].add(entry.getKey());
+        }
+
+        int capacity = Math.min(k, frequencyMap.size());
+        int[] result = new int[capacity];
+
+        int resultIndex = 0;
+
+        for (int i = buckets.length - 1; i >=0 && resultIndex < k; i--) {
+            if(buckets[i] != null) {
+                for (int num : buckets[i]) {
+                    result[resultIndex++] = num;
+                    if (resultIndex == k)
+                        break;
+                }
+            }
+        }
 
         return result;
     }
